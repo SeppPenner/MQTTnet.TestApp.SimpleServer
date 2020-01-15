df6854c7dac8672ef0bf237d1c0cac6aafb605a6
SeppPenner <HansM2013@gmx.de>
2020-01-15 18:10:46 +0100

    Initial commit.

---
+++ MQTTnet.TestApp.SimpleServer/Form1.cs
@@ -0,0 +1,221 @@
+﻿namespace MQTTnet.TestApp.SimpleServer
+{
+    using System;
+    using System.Text;
+    using System.Timers;
+    using System.Windows.Forms;
+
+    using MQTTnet.Client.Connecting;
+    using MQTTnet.Client.Disconnecting;
+    using MQTTnet.Client.Options;
+    using MQTTnet.Extensions.ManagedClient;
+    using MQTTnet.Formatter;
+    using MQTTnet.Protocol;
+    using MQTTnet.Server;
+
+    using Timer = System.Timers.Timer;
+
+    public partial class Form1 : Form
+    {
+
+        private readonly Timer timer;
+
+        private IManagedMqttClient managedMqttClientPublisher;
+
+        private IManagedMqttClient managedMqttClientSubscriber;
+
+        private IMqttServer mqttServer;
+
+        private string port = "1883";
+
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            this.timer = new Timer
+            {
+                AutoReset = true, Enabled = true, Interval = 1000
+            };
+            this.timer.Elapsed += this.Timer_Elapsed;
+        }
+
+        private static void OnPublisherConnected(MqttClientConnectedEventArgs x)
+        {
+            MessageBox.Show("Connected", "ConnectHandler", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private static void OnPublisherDisconnected(MqttClientDisconnectedEventArgs x)
+        {
+            MessageBox.Show("Disconnected", "ConnectHandler", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ButtonGeneratePublishedMessage_Click(object sender, EventArgs e)
+        {
+            var message = $"{{\"dt\":\"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}\"}}";
+            this.TextBoxPublish.Text = message;
+        }
+
+        private async void ButtonPublish_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var payload = Encoding.UTF8.GetBytes(this.TextBoxPublish.Text);
+
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(this.TextBoxTopic.Text.Trim()).WithPayload(payload).WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce).WithRetainFlag().Build();
+
+                if (this.managedMqttClientPublisher != null)
+                {
+                    await this.managedMqttClientPublisher.PublishAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Occurs", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void ButtonPublisherStart_Click(object sender, EventArgs e)
+        {
+            var mqttFactory = new MqttFactory();
+
+            var tlsOptions = new MqttClientTlsOptions
+            {
+                UseTls = false, IgnoreCertificateChainErrors = true, IgnoreCertificateRevocationErrors = true, AllowUntrustedCertificates = true
+            };
+
+            var options = new MqttClientOptions
+            {
+                ClientId = "ClientPublisher",
+                ProtocolVersion = MqttProtocolVersion.V311,
+                ChannelOptions = new MqttClientTcpOptions
+                {
+                    Server = "localhost", Port = int.Parse(this.TextBoxPort.Text.Trim()), TlsOptions = tlsOptions
+                }
+            };
+
+            if (options.ChannelOptions == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            options.CleanSession = true;
+            options.KeepAlivePeriod = TimeSpan.FromSeconds(5);
+
+            this.managedMqttClientPublisher = mqttFactory.CreateManagedMqttClient();
+            this.managedMqttClientPublisher.UseApplicationMessageReceivedHandler(this.HandleReceivedApplicationMessage);
+            this.managedMqttClientPublisher.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnPublisherConnected);
+            this.managedMqttClientPublisher.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnPublisherDisconnected);
+
+            await this.managedMqttClientPublisher.StartAsync(
+                new ManagedMqttClientOptions
+                {
+                    ClientOptions = options
+                });
+        }
+
+        private async void ButtonPublisherStop_Click(object sender, EventArgs e)
+        {
+            if (this.managedMqttClientPublisher == null)
+            {
+                return;
+            }
+
+            await this.managedMqttClientPublisher.StopAsync();
+            this.managedMqttClientPublisher = null;
+        }
+
+        private async void ButtonServerStart_Click(object sender, EventArgs e)
+        {
+
+            if (this.mqttServer != null)
+            {
+                return;
+            }
+
+            var storage = new JsonServerStorage();
+            storage.Clear();
+
+            this.mqttServer = new MqttFactory().CreateMqttServer();
+
+            var options = new MqttServerOptions();
+            options.DefaultEndpointOptions.Port = int.Parse(this.TextBoxPort.Text);
+            options.Storage = storage;
+            options.EnablePersistentSessions = true;
+
+            try
+            {
+                await this.mqttServer.StartAsync(options);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Occurs", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                await this.mqttServer.StopAsync();
+                this.mqttServer = null;
+            }
+        }
+
+        private async void ButtonServerStop_Click(object sender, EventArgs e)
+        {
+            if (this.mqttServer == null)
+            {
+                return;
+            }
+
+            await this.mqttServer.StopAsync();
+            this.mqttServer = null;
+        }
+
+        private async void ButtonSubscriberStart_Click(object sender, EventArgs e)
+        {
+            var options = new ManagedMqttClientOptionsBuilder().WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(new MqttClientOptionsBuilder().WithClientId("ClientSubscriber").WithTcpServer("localhost", int.Parse(this.TextBoxPort.Text)).WithTls().Build()).Build();
+
+            this.managedMqttClientSubscriber = new MqttFactory().CreateManagedMqttClient();
+            this.managedMqttClientSubscriber.UseApplicationMessageReceivedHandler(this.HandleReceivedApplicationMessage);
+            await this.managedMqttClientSubscriber.SubscribeAsync(new TopicFilterBuilder().WithTopic(this.TextBoxTopic.Text.Trim()).Build());
+            await this.managedMqttClientSubscriber.StartAsync(options);
+        }
+
+        private void HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            var item = $"Timestamp: {DateTime.Now:O} | Topic: {eventArgs.ApplicationMessage.Topic} | Payload: {eventArgs.ApplicationMessage.ConvertPayloadToString()} | QoS: {eventArgs.ApplicationMessage.QualityOfServiceLevel}";
+
+            this.BeginInvoke((MethodInvoker)delegate { this.TextBoxSubscriber.Text = item + Environment.NewLine + this.TextBoxSubscriber.Text; });
+        }
+
+        private void TextBoxPort_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(this.TextBoxPort.Text, out var p))
+            {
+                this.port = this.TextBoxPort.Text.Trim();
+            }
+            else
+            {
+                this.TextBoxPort.Text = this.port;
+                this.TextBoxPort.SelectionStart = this.TextBoxPort.Text.Length;
+                this.TextBoxPort.SelectionLength = 0;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.BeginInvoke(
+                (MethodInvoker)delegate
+                {
+                    // Server
+                    this.TextBoxPort.Enabled = this.mqttServer == null;
+                    this.ButtonServerStart.Enabled = this.mqttServer == null;
+                    this.ButtonServerStop.Enabled = this.mqttServer != null;
+
+                    // Publisher
+                    this.ButtonPublisherStart.Enabled = this.managedMqttClientPublisher == null;
+                    this.ButtonPublisherStop.Enabled = this.managedMqttClientPublisher != null;
+
+                    // Subscriber
+                    this.ButtonSubscriberStart.Enabled = this.managedMqttClientSubscriber == null;
+                    this.ButtonSubscriberStop.Enabled = this.managedMqttClientSubscriber != null;
+                });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+MQTTnet.TestApp.SimpleServer
+====================================
+
+MQTTnet.TestApp.SimpleServer is a test application to test [MQTTnet](https://github.com/chkr1011/MQTTnet).
+The assembly was written and tested in .Net Core 3.1 and originally created by [@grammyleung](https://github.com/grammyleung).
+
+[![Build status](https://ci.appveyor.com/api/projects/status/1j2g0x4y6xahj3pe?svg=true)](https://ci.appveyor.com/project/SeppPenner/mqttnet-testapp-simpleserver)
+[![GitHub issues](https://img.shields.io/github/issues/SeppPenner/MQTTnet.TestApp.SimpleServer.svg)](https://github.com/SeppPenner/MQTTnet.TestApp.SimpleServer/issues)
+[![GitHub forks](https://img.shields.io/github/forks/SeppPenner/MQTTnet.TestApp.SimpleServer.svg)](https://github.com/SeppPenner/MQTTnet.TestApp.SimpleServer/network)
+[![GitHub stars](https://img.shields.io/github/stars/SeppPenner/MQTTnet.TestApp.SimpleServer.svg)](https://github.com/SeppPenner/MQTTnet.TestApp.SimpleServer/stargazers)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://raw.githubusercontent.com/SeppPenner/MQTTnet.TestApp.SimpleServer/master/License.txt)
+[![Known Vulnerabilities](https://snyk.io/test/github/SeppPenner/MQTTnet.TestApp.SimpleServer/badge.svg)](https://snyk.io/test/github/SeppPenner/MQTTnet.TestApp.SimpleServer)
+
+Change history
+--------------
+
+* **Version 1.0.0.0 (2020-01-15)** : 1.0 release.
