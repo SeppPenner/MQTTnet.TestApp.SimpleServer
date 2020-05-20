223512f058e254ac797cafad16e28d47ce57a2c7
SeppPenner <HansM2013@gmx.de>
2020-05-20 18:52:47 +0200

    Updated nuget packages, fixed code style.

--- MQTTnet.TestApp.SimpleServer/Form1.cs
+++ MQTTnet.TestApp.SimpleServer/Form1.cs
@@ -1,4 +1,13 @@
-﻿namespace MQTTnet.TestApp.SimpleServer
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Haemmer Electronics">
+//   Copyright (c) 2020 All rights reserved.
+// </copyright>
+// <summary>
+//   The main form.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MQTTnet.TestApp.SimpleServer
 {
     using System;
     using System.Text;
@@ -15,16 +24,34 @@
 
     using Timer = System.Timers.Timer;
 
+    /// <summary>
+    /// The main form.
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// The managed publisher client.
+        /// </summary>
         private IManagedMqttClient managedMqttClientPublisher;
 
+        /// <summary>
+        /// The managed subscriber client.
+        /// </summary>
         private IManagedMqttClient managedMqttClientSubscriber;
 
+        /// <summary>
+        /// The MQTT server.
+        /// </summary>
         private IMqttServer mqttServer;
 
+        /// <summary>
+        /// The port.
+        /// </summary>
         private string port = "1883";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
             this.InitializeComponent();
@@ -37,22 +64,40 @@
             timer.Elapsed += this.Timer_Elapsed;
         }
 
+        /// <summary>
+        /// Handles the publisher connected event.
+        /// </summary>
+        /// <param name="x">The MQTT client connected event args.</param>
         private static void OnPublisherConnected(MqttClientConnectedEventArgs x)
         {
             MessageBox.Show("Connected", "ConnectHandler", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        /// <summary>
+        /// Handles the publisher disconnected event.
+        /// </summary>
+        /// <param name="x">The MQTT client disconnected event args.</param>
         private static void OnPublisherDisconnected(MqttClientDisconnectedEventArgs x)
         {
             MessageBox.Show("Disconnected", "ConnectHandler", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        /// <summary>
+        /// The method that handles the button click to generate a message.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private void ButtonGeneratePublishedMessage_Click(object sender, EventArgs e)
         {
             var message = $"{{\"dt\":\"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}\"}}";
             this.TextBoxPublish.Text = message;
         }
 
+        /// <summary>
+        /// The method that handles the button click to publish a message.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonPublish_Click(object sender, EventArgs e)
         {
             try
@@ -73,6 +118,11 @@
             }
         }
 
+        /// <summary>
+        /// The method that handles the button click to start the publisher.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonPublisherStart_Click(object sender, EventArgs e)
         {
             var mqttFactory = new MqttFactory();
@@ -112,6 +162,11 @@
                 });
         }
 
+        /// <summary>
+        /// The method that handles the button click to stop the publisher.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonPublisherStop_Click(object sender, EventArgs e)
         {
             if (this.managedMqttClientPublisher == null)
@@ -123,9 +178,13 @@
             this.managedMqttClientPublisher = null;
         }
 
+        /// <summary>
+        /// The method that handles the button click to start the server.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonServerStart_Click(object sender, EventArgs e)
         {
-
             if (this.mqttServer != null)
             {
                 return;
@@ -153,6 +212,11 @@
             }
         }
 
+        /// <summary>
+        /// The method that handles the button click to stop the server.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonServerStop_Click(object sender, EventArgs e)
         {
             if (this.mqttServer == null)
@@ -164,6 +228,11 @@
             this.mqttServer = null;
         }
 
+        /// <summary>
+        /// The method that handles the button click to start the subscriber.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private async void ButtonSubscriberStart_Click(object sender, EventArgs e)
         {
             var options = new ManagedMqttClientOptionsBuilder().WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
@@ -175,6 +244,10 @@
             await this.managedMqttClientSubscriber.StartAsync(options);
         }
 
+        /// <summary>
+        /// Handles the received application message event.
+        /// </summary>
+        /// <param name="eventArgs">The event args.</param>
         private void HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs eventArgs)
         {
             var item = $"Timestamp: {DateTime.Now:O} | Topic: {eventArgs.ApplicationMessage.Topic} | Payload: {eventArgs.ApplicationMessage.ConvertPayloadToString()} | QoS: {eventArgs.ApplicationMessage.QualityOfServiceLevel}";
@@ -182,8 +255,14 @@
             this.BeginInvoke((MethodInvoker)delegate { this.TextBoxSubscriber.Text = item + Environment.NewLine + this.TextBoxSubscriber.Text; });
         }
 
+        /// <summary>
+        /// The method that handles the text changes in the text box.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private void TextBoxPort_TextChanged(object sender, EventArgs e)
         {
+            // ReSharper disable once StyleCop.SA1126
             if (int.TryParse(this.TextBoxPort.Text, out _))
             {
                 this.port = this.TextBoxPort.Text.Trim();
@@ -196,6 +275,11 @@
             }
         }
 
+        /// <summary>
+        /// The method that handles the timer events.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             this.BeginInvoke(
